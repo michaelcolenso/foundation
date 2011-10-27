60beadd9f551afbd5d4696686d7c6edd3e2745d7
michaelcolenso <github@michaelcolenso.com>
2011-10-27 01:10:25 -0700

    rails guide - getting started 6.7

---
+++ jack/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jack::Application.config.secret_token = '054a86ec6c50af96a324a26543189d8e37ed8518283878a9093ffc3d8939fdb64431a8aa965a7d7890808f7039d358454b59fb217c5621ff9c48807cdd858552'
